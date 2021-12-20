9e41a993af859a55e6e047fcea0761b494b153cf
Sly <80170863+SlyVTT@users.noreply.github.com>
2021-12-20 21:12:38 +0100

    first commit, corresponding to rev0.9b on Casio Graph90+E

---
+++ Globals/GlobalFunctions.cpp
@@ -0,0 +1,110 @@
+#include "GlobalFunctions.hpp"
+
+#include <sys/stat.h>
+
+bool is_existing( const std::string& file )
+{
+    struct stat buffer;
+    return (stat (file.c_str(), &buffer) == 0);
+}
+
+
+
+
+#if RENDER_WITH_SDL == 1
+
+uint32_t getpixel(SDL_Surface *surface, int x, int y)
+{
+       int bpp = surface->format->BytesPerPixel;
+       // Here p is the address to the pixel we want to retrieve
+       uint8_t *p = (uint8_t *)surface->pixels + y * surface->pitch + x * bpp;
+
+       switch(bpp)
+       {
+       case 1:
+              return *p;
+
+       case 2:
+              return *(uint16_t *)p;
+
+       case 3:
+              if(SDL_BYTEORDER == SDL_BIG_ENDIAN)
+                     return p[0] << 16 | p[1] << 8 | p[2];
+              else
+                     return p[0] | p[1] << 8 | p[2] << 16;
+
+       case 4:
+              return *(uint32_t *)p;
+
+       default:
+              return 0;       // shouldn't happen, but avoids warnings
+       }
+}
+
+#else
+
+
+imagegc* ReadBMP( std::string filename)
+{
+    FILE* f = fopen(filename.c_str(), "rb");
+
+    if(f == NULL)
+        return nullptr;
+
+    unsigned char info[54];
+    fread(info, sizeof(unsigned char), 54, f); // read the 54-byte header
+
+    imagegc* image = (imagegc*) malloc( sizeof(imagegc));
+
+    // extract image height and width from header
+    int width = *(int*)&info[18];
+    int height = *(int*)&info[22];
+
+    image->width = width;
+    image->height = height;
+    uint16_t* imagedata = (uint16_t*) malloc( width * height * sizeof(uint16_t) );
+
+    int row_padded = (width*3 + 3) & (~3);
+    unsigned char* data = new unsigned char[row_padded];
+    char R,G,B;
+
+    //----------------------------
+    int offsetdata = *(int*)&info[10];
+    int delta;
+
+
+    if (offsetdata>54)
+    {
+        delta = offsetdata-54;
+        fread(data, sizeof(unsigned char), delta, f );
+    }
+    //-----------------------------
+
+    for(int i = 0; i < height; i++)
+    {
+        fread(data, sizeof(unsigned char), row_padded, f);
+        for(int j = 0; j < width*3; j += 3)
+        {
+            B=data[j];
+            G=data[j+1];
+            R=data[j+2];
+
+             uint16_t b = (B >> 3) & 0x1f;
+             uint16_t g = ((G >> 2) & 0x3f) << 5;
+             uint16_t r = ((R >> 3) & 0x1f) << 11;
+
+             imagedata[(height-1-i)*width+j/3] = (uint16_t) (r | g | b);
+        }
+    }
+    delete[] data;
+
+    image->data = imagedata;
+
+    fclose(f);
+    return image;
+}
+
+
+#endif
+
+
---
+++ structs.h
@@ -0,0 +1,235 @@
+#define NB_RECEPTORS_PER_DOOR_MAX 3
+
+enum orientations
+{
+    GAUCHE = 0,
+    DROITE,
+    HAUT,
+    BAS,
+    AUCUNE,
+    VERTICAL,
+    HORIZONTAL
+};
+
+struct Receptor
+{
+    unsigned char x;
+    unsigned char y;
+    orientations direction;
+    unsigned char R, G, B;
+    bool isvalide;
+};
+
+enum selection
+{
+    NONE = 0,
+    JOUEUR,
+    LUMIERE,
+    ROUGE,
+    VERT,
+    BLEU,
+    NOIR
+};
+
+enum ID
+{
+    VIDE = 0,
+    MUR,
+    PORTE
+};
+
+enum kind
+{
+    OPAQUE = 0,
+    TRANSPARENT = 1
+};
+
+struct Blocks
+{
+    unsigned char x;
+    unsigned char y;
+    unsigned char R,G,B,A;
+    kind type;
+};
+
+struct Player
+{
+    unsigned char x;
+    unsigned char y;
+    orientations direction;
+};
+
+struct MapItem
+{
+    unsigned char ID;
+    unsigned char x;
+    unsigned char y;
+    orientations direction;
+};
+
+struct Light
+{
+    unsigned char x;
+    unsigned char y;
+    unsigned char R,G,B,A;
+    bool movable;
+};
+
+struct Wall
+{
+    orientations direction;
+    unsigned int mini;
+    unsigned int maxi;
+    unsigned int fixe;
+};
+
+struct Door
+{
+    unsigned char x, y;
+    orientations direction;
+    bool isopen;
+    bool isdoor;
+    unsigned char nbreceptor;
+    signed char receptors[NB_RECEPTORS_PER_DOOR_MAX];
+    unsigned char nextLevelNumber;
+};
+
+enum content
+{
+    RIEN = 0,
+    PIERRE_NOIRE,
+    PIERRE_VERTE,
+    PIERRE_BLEUE,
+    PIERRE_ROUGE,
+    PIERRE_BLANCHE,
+    LINGOT_FER,
+    LINGOT_BRONZE,
+    LINGOT_ARGENT,
+    LINGOT_OR,
+    LINGOT_PLATINE,
+    WHITE_FRAGMENT,
+    BLACK_FRAGMENT ,
+    RED_FRAGMENT,
+    GREEN_FRAGMENT,
+    BLUE_FRAGMENT,
+    HEART_SINGLE,       // to restore one single point of Life (1 heart)
+    HEART_FULL              // to restore all points of Life (up to LifeMax, function of difficulty)
+};
+
+enum chest
+{
+    T_RED = 0,
+    T_YELLOW,
+    T_GREEN,
+    T_BLUE
+};
+
+enum nonmovable
+{
+    BLOCK_STONE = 0,
+    BLOCK_MOON,
+    BLOCK_WOOD,
+    BLOCK_SUN,
+    BLOCK_WATER
+};
+
+struct Obstacle
+{
+    unsigned char x, y;
+    nonmovable type;
+};
+
+struct Treasure
+{
+    unsigned char x, y;
+    chest type;
+    bool isopen;
+    content  inside;
+    unsigned int scoreboost;
+    bool isvisible;
+};
+
+struct Minimap
+{
+    signed char x, y;
+    unsigned char R,G,B,A;
+    bool visited;
+    bool doorNorth;
+    bool doorSouth;
+    bool doorWest;
+    bool doorEast;
+};
+
+struct LevelSaveStatus
+{
+    unsigned char chestStatusSave[8]={0xAA,0xAA,0xAA,0xAA,0xAA,0xAA,0xAA,0xAA};              // max 8 chest per level : status = 0 if chest not touched yet / 1 if open (and then empty) (activated one time) / 2 if not visible (activated 2 times)
+    unsigned char doorStatusSave[4]={0xBB,0xBB,0xBB,0xBB};                  // status of the door (0 = closed; 1 = open )
+    unsigned char bossStatusSave[8]={0xCC,0xCC,0xCC,0xCC,0xCC,0xCC,0xCC,0xCC};               // max 8 bosses per level (0 = normal; 1 = defeated )
+    unsigned char monsterStatusSave[8]={0xDD,0xDD,0xDD,0xDD,0xDD,0xDD,0xDD,0xDD};               // max 8 monsters per level (0 = normal; 1 = defeated )
+    unsigned char redBlockSave[2]={0xEE,0xEE};                          // X and Y coordinates
+    unsigned char greenBlockSave[2]={0xEE,0xEE};                          // X and Y coordinates
+    unsigned char blueBlockSave[2]={0xEE,0xEE};                          // X and Y coordinates
+    unsigned char blackBlockSave[2]={0xEE,0xEE};                          // X and Y coordinates
+    unsigned char lightSave[2]={0xFF,0xFF};                          // X and Y coordinates
+}; // TOTAL 38 bytes per level saved
+
+struct GameSaveStatus
+{
+    unsigned char currentScore[2];          // Score between 0 and 65535 [byte 1 : score / 256] and [byte 2 : score % 256 ]
+    unsigned char currentLevel;
+    unsigned char visitedLevels[33];        // Levels 1 to 32 save status as level 0 is always visited (0 = not visited, 1 = visited)
+    unsigned char currentDifficulty;
+    unsigned char currentLife;
+    unsigned char redFrag;                      // 0 = bot collected yet - 1 = collected
+    unsigned char greenFrag;                      // 0 = bot collected yet - 1 = collected
+    unsigned char blueFrag;                      // 0 = bot collected yet - 1 = collected
+    unsigned char blackFrag;                      // 0 = bot collected yet - 1 = collected
+    unsigned char whiteFrag;                      // 0 = bot collected yet - 1 = collected
+    unsigned char terminator = 0x99;
+};  // TOTAL 44 bytes
+
+enum monstertype
+{
+    BLOB = 0,
+    BAT,
+    GHOST,
+    SPIDER,
+    SKELETON
+};
+
+struct Monster
+{
+    unsigned char xcur, ycur;
+    unsigned char mini, maxi;
+    orientations direction;
+    orientations sens;
+    monstertype kind;
+};
+
+enum bosscolor
+{
+    B_RED = 0,
+    B_GREEN,
+    B_BLUE,
+    B_BLACK,
+    BIGBOSS
+};
+
+
+struct Boss
+{
+    unsigned char xcur, ycur;
+    unsigned char mini, maxi;
+    orientations direction;
+    orientations sens;
+    bosscolor color;
+};
+
+
+struct Bullet
+{
+    unsigned int xpart;
+    unsigned int ypart;
+    unsigned int color;
+    bool visible;
+};
